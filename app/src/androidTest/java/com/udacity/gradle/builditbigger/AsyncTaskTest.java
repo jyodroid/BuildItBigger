72cf04457267dfa7c6cc48089584f6bdf940794b
John Tangarife <jyodroid@localhost.localdomain>
2016-04-10 02:17:37 -0500

    async task test corrections

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -12,21 +12,23 @@ import java.util.concurrent.ExecutionException;
 /**
  * Created by jyodroid on 4/9/16.
  */
-public class AsyncTaskTest extends AndroidTestCase{
-    public void testVerifyAsyncTaskResponse(){
+public class AsyncTaskTest extends AndroidTestCase {
+    public void testVerifyAsyncTaskResponse() {
 
         //Arrange
         Random randomJokeNumber = new Random();
-
+        final String EMPTY_STRING = "";
         JokesTeller jokesTeller = new JokesTeller();
 
         int jokeNumber = randomJokeNumber.nextInt(jokesTeller.getJokesSize());
 
-        String joke = "";
+        String joke = EMPTY_STRING;
 
         //Act
         try {
-             joke = new BackEndConnectTask().execute(new Pair<Context, Integer>(getContext(), jokeNumber)).get();
+            joke =
+                    new BackEndConnectTask()
+                            .execute(new Pair<Context, Integer>(getContext(), jokeNumber)).get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
@@ -34,6 +36,6 @@ public class AsyncTaskTest extends AndroidTestCase{
         }
 
         //Assert
-        assertNotSame("Joke is "+joke,joke, "");
+        assertFalse("Joke is " + joke, joke.equals(EMPTY_STRING));
     }
 }
